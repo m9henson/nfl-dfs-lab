62809b9614b590c3b9973b0b9323ca72412a84fd
m9henson <m9henson@gmail.com>
2026-08-07 10:44:37 -0500

    Add files via upload

--- pages/+config.ts
+++ pages/+config.ts
@@ -3,7 +3,6 @@ import vikeReact from 'vike-react/config'
 
 export default {
   extends: [vikeReact],
-  ssr: false,
   title: 'NFL DFS Lab',
   description: 'NFL DraftKings research and lineup optimizer'
 } satisfies Config
