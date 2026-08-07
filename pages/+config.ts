09af23db1882e4e7c256567c139c09e25d2a1441
m9henson <m9henson@gmail.com>
2026-08-07 11:10:02 -0500

    Delete pages/+config.ts

--- pages/+config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from 'vike/types'
-import vikeReact from 'vike-react/config'
-
-export default {
-  extends: [vikeReact],
-  title: 'NFL DFS Lab',
-  description: 'NFL DraftKings research and lineup optimizer',
-  viewport: 'width=device-width, initial-scale=1, viewport-fit=cover'
-} satisfies Config
